5399b5dd6a1b6bb2df13ab512a4b4fdf23fa3c9c
Ladiii-PC-rose <ladis13.bodele@gmail.com>
2020-09-14 18:36:29 +0200

    Adieu log_line.php :)

--- log_line.php
+++
@@ -1,6 +0,0 @@
-<div class="log-line">
-    <label class="log-stage"></label>
-    <label class="log-calc"></label>
-    <label class="log-response"></label>
-    <label class="log-status"></label>
-</div>
